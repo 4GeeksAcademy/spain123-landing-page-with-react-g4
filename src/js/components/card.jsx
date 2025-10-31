0caf2e55356aec38aef61df2627ebd1cfff2f699
PicazoXV <apicazovilla@gmail.com>
2025-10-31 14:20:48 +0100

    feat: Card

--- src/js/components/card.jsx
+++ src/js/components/card.jsx
@@ -1,19 +1,14 @@
 import React from "react";
 
-export const Card = () => {
-  let person = { name: "", url: "", description: "" };
-
+export const Card = (pokemon) => {
   return (
     <div>
-      <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="" alt="Card image cap" />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" class="btn btn-primary">
+      <div className="card" style="width: 18rem;">
+        <img src={pokemon.url} alt="Card image cap" />
+        <div className="card-body">
+          <h5 className="card-title">{pokemon.name}</h5>
+          <p className="card-text">{pokemon.description}</p>
+          <a href="#" className="btn btn-primary">
             Go somewhere
           </a>
         </div>
